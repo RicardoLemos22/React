f29cd3e34376812f6e6312121788de3c9b1c5df6
ricardo lemos <l_riki@hotmail.com>
2023-07-27 22:04:40 -0300

    entrega1: subida2

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,17 @@
-import './App.css'
+// import './App.css'
+import {NavBar} from "./components/NavBar.jsx"
+
+
+const Texto = ({ parrafo }) => {
+  return <p>{parrafo}</p>
+}
 
 function App() {
-  const arreglo = [1,2,3,"coder","react"]
-
-  
-  return <div>
-    {
-       arreglo.map(elemento => {
-        console.log(elemento)
-        return <p key={elemento}>{elemento}</p>
-        /* hay que usar llaves para q muestre el valor de la variable*/
-       } )
-    }
-    {
-       arreglo2.map(elemento => {
-        console.log(elemento)
-        return <p key={elemento}>{elemento}</p>
-        /* hay que usar llaves para q muestre el valor de la variable*/
-       } )
-    }
-  </div>
-      
+  return (
+    <>
+      <Texto parrafo="Hola coders"></Texto>
+    </>
+  )
 }
 
 export default App
