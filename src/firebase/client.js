f257ce51088551ad9f59c537578d7d93ba1a045d
ricardo lemos <l_riki@hotmail.com>
2023-09-02 21:45:16 -0300

    entrega3: test git

---
+++ src/firebase/client.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC7R4UnlK--AJ3mbRXvGuFY4CVJsiKPKww",
+  authDomain: "beauty-elixir.firebaseapp.com",
+  projectId: "beauty-elixir",
+  storageBucket: "beauty-elixir.appspot.com",
+  messagingSenderId: "834427611803",
+  appId: "1:834427611803:web:e79e8fce4cadac8371343e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
